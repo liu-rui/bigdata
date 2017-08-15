1a3b4edee7bfefbd209b3498fc2656127b642a2b
liurui <liurui@lingyi365.com>
2017-08-15 16:23:18 +0800

    实现map reduce统计

---
+++ bigdatademo/src/main/java/App.java
@@ -0,0 +1,71 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class App {
+    public static class MyMapper extends Mapper<Object, Text, Text, IntWritable> {
+        @Override
+        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer stringTokenizer = new StringTokenizer(value.toString());
+            context.write(new Text(stringTokenizer.nextToken()), new IntWritable(Integer.parseInt(stringTokenizer.nextToken())));
+        }
+    }
+
+    public static class MyReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            int count = 0;
+            for (IntWritable value : values) {
+                sum += value.get();
+                count++;
+            }
+
+            context.write(key, new IntWritable(sum / count));
+        }
+    }
+
+    public static class MyTool implements Tool {
+        private Configuration _conf;
+
+        public int run(String[] args) throws Exception {
+            Job job = Job.getInstance(_conf);
+
+            job.setJobName("test");
+            job.setJarByClass(App.class);
+            job.setMapperClass(MyMapper.class);
+            job.setCombinerClass(MyReduce.class);
+            job.setReducerClass(MyReduce.class);
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(IntWritable.class);
+
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+            return job.waitForCompletion(true) ? 0 : 1;
+        }
+
+        public void setConf(Configuration conf) {
+            _conf = conf;
+        }
+
+        public Configuration getConf() {
+            return _conf;
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        System.exit(ToolRunner.run(new MyTool(), args));
+    }
+}
